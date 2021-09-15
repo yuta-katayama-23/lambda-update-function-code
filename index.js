e7159913a4de7142b33daa262f0bfa04c5136c03
Yuta Katayama <yutapilafu@gmail.com>
2021-09-15 21:26:37 +0900

    objectのファイル名を確認するように変更

--- index.js
+++ index.js
@@ -4,27 +4,40 @@ const client = new LambdaClient({ region: process.env.REGION });
 exports.handler = async (event) => {
     try {
         const bucket = event.Records[0].s3.bucket.name;
-        const updateInput = {
-            FunctionName: process.env.FUNCTION_NAME,
-            S3Bucket: bucket,
-            S3Key: process.env.FILE_NAME
-        };
-        const publishInput = {
-            FunctionName: process.env.FUNCTION_NAME
-        };
+        const object = event.Records[0].s3.object.key;
 
-        const updateCommand = new UpdateFunctionCodeCommand(updateInput);
-        const updateResponse = await client.send(updateCommand);
-        console.log("UpdateFunctionCodeCommand status", updateResponse.$metadata.httpStatusCode);
-        console.log("state", updateResponse.State);
+        if (object.includes(process.env.FILE_NAME)) {
+            const updateInput = {
+                FunctionName: process.env.FUNCTION_NAME,
+                S3Bucket: bucket,
+                S3Key: object
+            };
 
-        if (process.env.PUBLISH) {
-            const publishCommand = new PublishVersionCommand(publishInput);
-            const publishResponse = await client.send(publishCommand);
-            console.log("PublishVersionCommand status", publishResponse.$metadata.httpStatusCode);
-            console.log("state", publishResponse.State);
-            return { status: publishResponse.$metadata.httpStatusCode, result: "ok", response: publishResponse }
-        } else return { status: updateResponse.$metadata.httpStatusCode, result: "ok", response: updateResponse }
+            const updateCommand = new UpdateFunctionCodeCommand(updateInput);
+            const updateResponse = await client.send(updateCommand);
+
+            console.log("UpdateFunctionCodeCommand status", updateResponse.$metadata.httpStatusCode);
+            console.log("UpdateFunctionCodeCommand state", updateResponse.State);
+
+            if (process.env.PUBLISH) {
+                const publishInput = {
+                    FunctionName: process.env.FUNCTION_NAME
+                };
+
+                const publishCommand = new PublishVersionCommand(publishInput);
+                const publishResponse = await client.send(publishCommand);
+
+                console.log("PublishVersionCommand status", publishResponse.$metadata.httpStatusCode);
+                console.log("PublishVersionCommand state", publishResponse.State);
+
+                return { status: publishResponse.$metadata.httpStatusCode, result: "ok", response: publishResponse };
+            } else {
+                return { status: updateResponse.$metadata.httpStatusCode, result: "ok", response: updateResponse };
+            }
+        }
+
+        console.log("not applicable. object is ", object);
+        return { status: 200, result: "not applicable" };
     } catch (error) {
         return errorHandler(error);
     }
@@ -32,13 +45,13 @@ exports.handler = async (event) => {
 
 const errorHandler = (error) => {
     const obj = {};
-    if (error.response) {
-        obj["status"] = error.response.status;
-        obj["statusText"] = error.response.statusText;
-        obj["data"] = error.response.data;
-    }
+    obj["status"] = 500;
     obj["message"] = error.message;
+    obj["stack"] = error.stack;
     obj["result"] = "ng";
+    if (error.$metadata) {
+        obj["status"] = error.$metadata.httpStatusCode;
+    }
 
     console.log("errorHandler", obj);
     return obj;
